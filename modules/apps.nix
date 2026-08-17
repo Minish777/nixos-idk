f66910c4a678f8098f611090f9e81e4d534b9978
steelium <steelium@email.com>
2026-08-17 12:41:13 +0300

    update nixpkgs

--- modules/apps.nix
+++ modules/apps.nix
@@ -59,6 +59,6 @@ programs.appimage = {
 	protonplus
 	ffmpeg
 	vlc
-	inputs.zen-browser.packages."${pkgs.system}".default
+    inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default
   ];
 }
